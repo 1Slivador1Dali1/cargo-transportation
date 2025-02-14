110e83db70bff618f17ac67cb89a000e7997db77
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2025-02-14 22:15:27 +0300

    Creating Hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import styles from "./index.module.scss";
+
+export default function Hero() {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.hero__left}>
+        <h1 className={styles.hero__left_title}>
+          Аренда спецтехники | Доставка строительных материалов
+        </h1>
+        <p className={styles.hero__left_text}>
+          Различные виды спецтехники под ваши нужды в г. Солнечногорск,
+          Солнечногорский р-он, г. Клин, г. Зеленоград, г. Истра, пос. Поварово,
+          Хоругвино, Есипово, Ложки.
+        </p>
+        <a href="tel:+79166181481" className={styles.hero__left_tel}>
+          Заказать услугу
+        </a>
+      </div>
+
+      <div className={styles.hero__right}>
+        <img
+          className={styles.hero__right_img}
+          src="src/assets/ivanovec1.webp"
+          alt="Вездеход камаз манипулятор"
+        />
+      </div>
+    </div>
+  );
+}
